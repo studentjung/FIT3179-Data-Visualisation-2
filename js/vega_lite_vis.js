c0ec93a9ad777727736e2208c7ee1307892ed0ce
Shung Jung <shungjung@outlook.com>
2025-10-13 11:17:25 +1100

    Added a lot of graphs

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,19 @@
-var spec_bar_chart = "js/daily_cost_bar_chart.vg.json";
-vegaEmbed("#bar_chart", spec_bar_chart).then(function(result) {
+var bar_chart = "js/landuse_bar.vg.json";
+vegaEmbed("#bar", bar_chart).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var map_chart = "js/landuse_pct_map.vg.json";
+vegaEmbed("#map", map_chart).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var bubble_plot = "js/landuse_pop_bubble.vg.json";
+vegaEmbed("#bubble", bubble_plot).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var dot_plot = "js/pop_change_dotplot.vg.json";
+vegaEmbed("#dot", dot_plot).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
