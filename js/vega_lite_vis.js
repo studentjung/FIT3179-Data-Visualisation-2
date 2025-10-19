bbc4e4573cdfbb1410d816ac902ed8e95076af27
Shung Jung <shungjung@outlook.com>
2025-10-19 15:20:36 +1100

    Done.

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,24 +1,52 @@
 var bar_chart = "js/landuse_bar.vg.json";
-vegaEmbed("#bar", bar_chart).then(function(result) {
+vegaEmbed("#bar", bar_chart, { actions: false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var map_chart = "js/landuse_pct_map.vg.json";
-vegaEmbed("#map", map_chart).then(function(result) {
+vegaEmbed("#map", map_chart, { actions: false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var bubble_plot = "js/landuse_pop_bubble.vg.json";
-vegaEmbed("#bubble", bubble_plot).then(function(result) {
+var scatter_plot = "js/landuse_pop_bubble.vg.json";
+vegaEmbed("#scatter", scatter_plot, { actions: false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var dot_plot = "js/pop_change_dotplot.vg.json";
-vegaEmbed("#dot", dot_plot).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+const bubble_container = document.getElementById("bubble");
+const bubble_width = bubble_container.clientWidth - 190;
+async function renderBubble() {
+    
+    const bubble_res = await fetch("js/landuse_migration_bubble.vg.json");
+    const bubble_spec = await bubble_res.json();
+    
+    const dyna_bubble_map = {
+        ...bubble_spec,
+        width: bubble_width / 3
+    };
+    
+    vegaEmbed("#bubble", dyna_bubble_map, { actions: false }).then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+}
 
-var tree_map = "js/pop_treemap.vg.json";
-vegaEmbed("#tree", tree_map).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+renderBubble()
+
+const tree_container = document.getElementById("tree");
+const tree_width = tree_container.clientWidth - 50;
+async function renderTreeMap() {
+    
+    const tree_res = await fetch("js/pop_treemap.vg.json");
+    const tree_spec = await tree_res.json();
+    
+    const dyna_tree_map = {
+        ...tree_spec,
+        width: tree_width
+    };
+    
+    vegaEmbed("#tree", dyna_tree_map, { actions: false }).then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+}
+
+renderTreeMap()
